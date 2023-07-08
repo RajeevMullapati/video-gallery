ec779e87a107a018390985f5ab13d5ac70deaffd
Rajeev <rajeevmullapati@gmail.com>
2023-07-08 15:22:53 +0530

    Modifed db files

--- Project/settings.py
+++ Project/settings.py
@@ -84,12 +84,9 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
 
