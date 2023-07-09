c7734285ebe7fb38faf46a62f0b94fb11421fe7e
Rajeev <rajeevmullapati@gmail.com>
2023-07-10 02:56:46 +0530

    updates

--- Project/settings.py
+++ Project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,9 +27,9 @@ CORS_ALLOWED_ORIGINS = [
 SECRET_KEY = 'django-insecure-gwn$9xkv+lug&-8rdty^tzh6n5=n3v^)q_su04i10fnpp9=gt6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','RajeevMullapati.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -125,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
