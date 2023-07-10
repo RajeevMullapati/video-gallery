c5abe19553ec26c37afaa89dd83aecfe33aee3b3
Rajeev <rajeevmullapati@gmail.com>
2023-07-10 12:11:32 +0530

    modifying settings

--- Project/settings.py
+++ Project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-gwn$9xkv+lug&-8rdty^tzh6n5=n3v^)q_su04i10fnpp9=gt6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', 'RajeevMullapati.pythonanywhere.com']
 
 
 # Application definition
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
